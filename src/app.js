be0def9d0920930f29f3ef7c1fba9b4fee06bf0d
ABDELKRIM <eljamiiabdelkarim@gmail.com>
2025-07-16 17:40:21 +0100

    Implémentation du contrôle d’accès par rôles dans l’API Gateway

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const authRoutes = require('./routes/auth.routes');
-
-app.use(express.json());
-app.use('/auth', authRoutes);
-
-module.exports = app; 
